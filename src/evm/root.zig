7a1e97bb71cbb9899f960b1c08a1b25b0897fb0b
Xearty <xeartybg@gmail.com>
2024-04-03 02:45:57 +0300

    refactor(execution): move `executeBytecode` to a separate file

--- src/evm/root.zig
+++ src/evm/root.zig
@@ -4,26 +4,4 @@ pub const Opcode = @import("opcodes.zig").Opcode;
 pub const Instruction = instructions.Instruction;
 pub const Context = @import("context.zig").Context;
 
-pub fn executeInstruction(ctx: *Context, instruction: *const Instruction) !void {
-    switch (instruction.*) {
-        .PUSH1 => |data| {
-            try ctx.stack.append(data.value);
-        },
-        .ADD => {
-            const operand1 = ctx.stack.popOrNull().?;
-            const operand2 = ctx.stack.popOrNull().?;
-            try ctx.stack.append(operand1 + operand2);
-        },
-        .STOP => {
-            ctx.is_halted = true;
-        }
-    }
-}
-
-pub fn executeBytecode(ctx: *Context, bytecode: []const u8) !void {
-    while (!ctx.is_halted and ctx.program_counter < bytecode.len) {
-        const instruction = instructions.decode(bytecode[ctx.program_counter..]);
-        ctx.program_counter += instructions.getSize(instruction);
-        try executeInstruction(ctx, &instruction);
-    }
-}
+pub const executeBytecode = @import("execution.zig").executeBytecode;
