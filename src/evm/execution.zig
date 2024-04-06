a2dc62425aa11d3b7580e87838a1a5d98569187a
Xearty <xeartybg@gmail.com>
2024-04-06 03:17:07 +0300

    fix(inst): use wrapping arithmetic for `ADDMOD` and `MULMOD`

--- src/evm/execution.zig
+++ src/evm/execution.zig
@@ -51,14 +51,14 @@ pub fn executeInstruction(ctx: *Context, instruction: *const Instruction) !void 
             const operand1 = ctx.stack.pop();
             const operand2 = ctx.stack.pop();
             const operand3 = ctx.stack.pop();
-            const result = (operand1 + operand2) % operand3;
+            const result = (operand1 +% operand2) % operand3;
             try ctx.stack.push(result);
         },
         .MULMOD => {
             const operand1 = ctx.stack.pop();
             const operand2 = ctx.stack.pop();
             const operand3 = ctx.stack.pop();
-            const result = (operand1 * operand2) % operand3;
+            const result = (operand1 *% operand2) % operand3;
             try ctx.stack.push(result);
         },
         .EXP => {
